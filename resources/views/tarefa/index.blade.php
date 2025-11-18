531bef5a27a2e3061cfb7319f48191a181f74434
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-18 18:56:15 -0300

    feat: adiciona novos links de navegação

--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -5,8 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Tarefas</div>
-
+                    <div class="card-header">Tarefas <a href="{{ route('tarefa.create') }}" class="float-end">Novo</a></div>
                     <div class="card-body">
                         <table class="table">
                             <thead>
@@ -25,11 +24,13 @@
                                         <td>{{ date('d/m/Y', strtotime($t['data_limite_conclusao'])) }}</td>
                                         <td><a href="{{ route('tarefa.edit', $t['id']) }}">Editar</a></td>
                                         <td>
-                                            <form id="form_{{ $t['id'] }}" method="post" action="{{ route('tarefa.destroy', ['tarefa' => $t['id']]) }}">
+                                            <form id="form_{{ $t['id'] }}" method="post"
+                                                action="{{ route('tarefa.destroy', ['tarefa' => $t['id']]) }}">
                                                 @method('DELETE')
                                                 @csrf
                                             </form>
-                                            <a href="#" onclick="document.getElementById('form_{{$t['id']}}').submit()">Excluir</a>
+                                            <a href="#"
+                                                onclick="document.getElementById('form_{{ $t['id'] }}').submit()">Excluir</a>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -38,15 +39,17 @@
 
                         <nav>
                             <ul class="pagination">
-                                <li class="page-item"><a class="page-link" href="{{ $tarefas->previousPageUrl() }}">Voltar</a></li>
+                                <li class="page-item"><a class="page-link"
+                                        href="{{ $tarefas->previousPageUrl() }}">Voltar</a></li>
 
-                                @for($i = 1; $i <= $tarefas->lastPage(); $i++)
-                                <li class="page-item {{ $tarefas->currentPage() == $i ? 'active' : '' }}">
-                                    <a class="page-link" href="{{ $tarefas->url($i) }}">{{ $i }}</a>
-                                </li>
+                                @for ($i = 1; $i <= $tarefas->lastPage(); $i++)
+                                    <li class="page-item {{ $tarefas->currentPage() == $i ? 'active' : '' }}">
+                                        <a class="page-link" href="{{ $tarefas->url($i) }}">{{ $i }}</a>
+                                    </li>
                                 @endfor
 
-                                <li class="page-item"><a class="page-link" href="{{ $tarefas->nextPageUrl() }}">Avançar</a></li>
+                                <li class="page-item"><a class="page-link" href="{{ $tarefas->nextPageUrl() }}">Avançar</a>
+                                </li>
                             </ul>
                         </nav>
                     </div>
