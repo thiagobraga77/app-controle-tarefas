989069d92bdeae00480c63c3e1c7a3dd044c477d
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-26 06:35:20 -0300

    feat: adiciona o pacote DOMPDF

--- config/app.php
+++ config/app.php
@@ -130,6 +130,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         // App\Providers\EventServiceProvider::class,
         // App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class
 
     ],
 
@@ -180,6 +181,7 @@ return [
 
         // Laravel Excel
         'Excel'     => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
