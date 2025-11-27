e4682710185fc2ac0d444b7ea030e941c812cf5e
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-27 06:42:46 -0300

    feat: adiciona estilização nos arquivos DOMPDF

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,21 +1,64 @@
-<h2>Lista de tarefas</h2>
+<html>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data limite conclusão</th>
-        </tr>
-    </thead>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+        .titulo {
+            border: 1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            width: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 25px;
+        }
+        .tabela {
+            width: 100%;
+        }
+        table th {
+            text-align: left;
+        }
 
-    <tbody>
-        @foreach ($tarefas as $key => $tarefa)
+    </style>
+</head>
+
+<body>
+
+
+    <div class="titulo">Lista de tarefas</div>
+
+    <table>
+        <thead>
             <tr>
-                <td>{{ $tarefa->id }}</td>
-                <td>{{ $tarefa->tarefa }}</td>
-                <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                <th>ID</th>
+                <th>Tarefa</th>
+                <th>Data limite conclusão</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+
+        <tbody>
+            @foreach ($tarefas as $key => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="page-break"></div>
+    <h2>Página 2</h2>
+
+    <div class="page-break"></div>
+    <h2>Página 3</h2>
+
+    {{-- <div class="page-break"></div>
+    <h2>Página 2</h2> --}}
+
+</body>
+
+</html>
